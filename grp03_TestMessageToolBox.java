4ee38a55be941be74d169cac462a00e4f76bd4de
elizamac <elizaf.macovei@gmail.com>
2023-11-27 10:53:25 +0000

    timeOfDatFormatted

--- grp03_TestMessageToolBox.java
+++ grp03_TestMessageToolBox.java
@@ -1,2 +1,6 @@
 public class grp03_TestMessageToolBox {
+  public static void main(String[] args) {
+    //!TESTING: timeOfDayFormatted
+    System.out.println(grp03_MessageToolBox.timeOfDayFormatted(System.currentTimeMillis()));
+  }
 }
