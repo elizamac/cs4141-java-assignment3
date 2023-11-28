3e6530dee8c4ea86bd55c91d149df6bc4926df2a
elizamac <elizaf.macovei@gmail.com>
2023-11-28 17:14:16 +0000

    testing age of post method

--- src/grp03_TestMessageToolBox.java
+++ src/grp03_TestMessageToolBox.java
@@ -1,6 +1,14 @@
 public class grp03_TestMessageToolBox {
   public static void main(String[] args) {
-    //!TESTING: timeOfDayFormatted
+    // !TESTING: timeOfDayFormatted
     System.out.println(grp03_MessageToolBox.timeOfDayFormatted(System.currentTimeMillis()));
+    
+    // !TESTING: post age
+    final long MILLISECONDS_IN_A_DAY = 24 * 60 * 60 * 1000;
+    long postTime = System.currentTimeMillis() - 3 * MILLISECONDS_IN_A_DAY - (1000 * 60 * 60) * 18;
+    System.out.println("Diff between current time (" + grp03_MessageToolBox.dayName(System.currentTimeMillis()) + ", "
+        + grp03_MessageToolBox.timeOfDayFormatted(System.currentTimeMillis()) + ") and post time ("
+        + grp03_MessageToolBox.dayName(postTime) + ", " + grp03_MessageToolBox.timeOfDayFormatted(postTime) + "): "
+        + grp03_MessageToolBox.ageOfPost(postTime) + " days");
   }
 }
